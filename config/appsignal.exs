b064e60c9db81e7a916dfcc67d48371e99a0103b
Brian Flanagan <brian@elbow.ie>
2023-01-15 09:53:49 +0000

    ibid

--- config/appsignal.exs
+++ config/appsignal.exs
@@ -4,5 +4,4 @@ config :appsignal, :config,
   otp_app: :price_register,
   name: "priceregister.civictech.ie",
   push_api_key: "b3b191f4-40c7-41f8-b540-48e5eb9ab642",
-  env: Mix.env
-
+  env: Mix.env()
