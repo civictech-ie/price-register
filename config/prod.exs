47f35af785988babc9689953e627e0fc938e7b60
Brian Flanagan <brian@elbow.ie>
2023-02-16 22:35:52 +0000

    ibid

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :price_register, PriceRegisterWeb.Endpoint,
-  # url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: :conn
 
 # Configures Swoosh API Client
