11c3306dd1944cc300b1b6a4d3e76e9134496561
brian <b@flngn.com>
2025-01-14 01:05:59 +0000

    changes to facilitate deployment on render.com

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Initial setup
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+MIX_ENV=prod mix assets.build
+MIX_ENV=prod mix assets.deploy
+
+MIX_ENV=prod mix phx.gen.release
+MIX_ENV=prod mix release --overwrite
