4c58efc765bc6f54e99b5abb8663ecaee2770afb
brian <b@flngn.com>
2025-01-17 17:16:07 +0000

    fix the property register facade

--- lib/ppr_api/property_register.ex
+++ lib/ppr_api/property_register.ex
@@ -2,14 +2,16 @@ defmodule PprApi.PropertyRegister do
   alias Floki
   alias Timex
 
-  @url "https://propertypriceregister.ie"
+  @url "https://www.propertypriceregister.ie/website/npsra/pprweb.nsf/PPR"
 
   def has_the_register_been_updated_since?(nil), do: true
 
   def has_the_register_been_updated_since?(time) do
+    time_in_zone = Timex.to_datetime(time, "Europe/Dublin")
+
     case fetch_last_update_time() do
       {:ok, last_update_time} ->
-        DateTime.compare(DateTime.from_naive!(last_update_time, "Etc/UTC"), time) == :gt
+        DateTime.compare(last_update_time, time_in_zone) == :gt
 
       {:error, _} ->
         false
@@ -17,9 +19,9 @@ defmodule PprApi.PropertyRegister do
   end
 
   defp fetch_last_update_time do
-    case HTTPoison.get(@url) do
-      {:ok, %{body: body}} ->
-        parse_last_update_time(body)
+    case HTTPoison.get(@url, [], hackney: [insecure: true]) do
+      {:ok, %{body: html}} ->
+        parse_last_update_time(html)
 
       {:error, reason} ->
         {:error, reason}
@@ -27,17 +29,22 @@ defmodule PprApi.PropertyRegister do
   end
 
   defp parse_last_update_time(html) do
-    {:ok, document} = Floki.parse_document(html)
+    {:ok, doc} = Floki.parse_document(html)
+    [update | _] = Floki.find(doc, "#LastUpdated")
+    [_, date_str] = Regex.run(~r/(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2})/, Floki.text(update))
 
-    case Floki.find(document, "div.well h4") do
-      [update_time | _] ->
-        update_time
-        |> Floki.text()
-        |> String.replace("REGISTER LAST UPDATED - ", "")
-        |> Timex.parse!("{0D}/{0M}/{YYYY} {h24}:{m}:{s}")
-        |> then(&{:ok, &1})
-
-      _ ->
+    with {:ok, document} <- Floki.parse_document(html),
+         [update_time | _] <- Floki.find(document, "#LastUpdated"),
+         update_time_text = Floki.text(update_time),
+         [_, datetime_str] <-
+           Regex.run(~r/(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2})/, update_time_text),
+         {:ok, naive_datetime} <-
+           Timex.parse(datetime_str, "{0D}/{0M}/{YYYY} {h24}:{m}:{s}"),
+         local_datetime <-
+           Timex.to_datetime(naive_datetime, "Europe/Dublin") do
+      {:ok, local_datetime |> Timex.to_datetime("Europe/Dublin")}
+    else
+      error ->
         {:error, :timestamp_not_found}
     end
   end
